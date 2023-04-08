ff3281eb253212c20789c72efb81d37534366fec
guilherme <guilhermedmlopes@hotmail.com>
2023-04-08 18:29:27 -0300

    Primeiros passos com JS no Front.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # paginasDinamicasJS
 
 ## Montagem de uma página dinâmica usando JavaScript para Front-End.
+
+## Separado por branches:
+
+- aula1: Entendemos um pouco mais sobre a pagina HTML, usar JS no proprio documento e seguir as boas praticas, criando o arquivo JS para realizar ações no HTML separadamente. Vimos como selecionar um elemento HTML em JS.
---
+++ main.js
@@ -0,0 +1,13 @@
+//Arquivo responsavel por realizar o dinamismo da pagina html
+
+//Para ligar o arquivo JS com o html
+//Inserimos no html a tag <script><\script>
+
+//criando alert ao acessar o arquivo html.
+//Assim que o arquivo html for executado.
+//alert('Ola mundo!');
+
+//document do JS é uma referencia ao documento em que realizaremos uma ação
+//Buscaremos a classe do botão 'pom' para realizar uma ação nele. Usa-se querySelector
+//Digitando o codigo no navegador, ele mostra o trecho do botão em html
+document.querySelector('.tecla_pom');
