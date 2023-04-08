3ea65996612558607dae0215dfb635f03d4686cf
guilherme <guilhermedmlopes@hotmail.com>
2023-04-08 18:58:41 -0300

    Adicionando arquivos referentes a executação de um som ao clicar em um botão da tela

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 ## Separado por branches:
 
-- aula1: Entendemos um pouco mais sobre a pagina HTML, usar JS no proprio documento e seguir as boas praticas, criando o arquivo JS para realizar ações no HTML separadamente. Vimos como selecionar um elemento HTML em JS.
+- aula1: Entendemos um pouco mais sobre a pagina HTML, usar JS no proprio documento e seguir as boas praticas, criando o arquivo JS para realizar ações no HTML separadamente. Vimos como selecionar um elemento HTML em JS. Também aprendemos a fazer o link do html com o arquivo JS usando a tag "script".
+
+- aula2: Entendemos que a tag script tem um local adequado para ser escrita no HTML. Vimos como executar o som ao clicar em um botão.
--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 //Arquivo responsavel por realizar o dinamismo da pagina html
 
-//Para ligar o arquivo JS com o html
-//Inserimos no html a tag <script><\script>
+//Os navegadores não deixam que sejam executados nenhuma ação de som, etc antes de uma interação do usuario
+//Para isso, precisaremos criar uma função para executar o som, quando houver alguma interação, no caso, apertar o botão pom
+function tocaSomPom() {
+    //Reproduzindo um som dentro da tag audio ao clicar em um botao
+    
+    //Acessando a tag audio pelo id e executando o audio
+    //Digitando o mesmo codigo no navegador, sera possivel ouvir o som do audio
+    document.querySelector('#som_tecla_pom').play();
+}
 
-//criando alert ao acessar o arquivo html.
-//Assim que o arquivo html for executado.
-//alert('Ola mundo!');
+//Executando função tocaSomPom ao clicar no botão com a classe '.tecla_pom'.
+//Removemos o parenteses da função tocaSomPom para que ela só seja executada ao clicar no botão
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-//document do JS é uma referencia ao documento em que realizaremos uma ação
-//Buscaremos a classe do botão 'pom' para realizar uma ação nele. Usa-se querySelector
-//Digitando o codigo no navegador, ele mostra o trecho do botão em html
-document.querySelector('.tecla_pom');
